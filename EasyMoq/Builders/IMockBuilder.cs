f6b0a9766773da1b8b0ded8217e808f6ad3cff26
Erik Dietrich <erik@daedtech.com>
2011-12-22 20:36:48 -0600

    Began refactoring the target builder into other classes and added a few more tests.

---
+++ EasyMoq/Builders/IMockBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+
+namespace DaedTech.EasyMoq.Builders
+{
+    /// <summary>This interface defines </summary>
+    public interface IMockBuilder
+    {
+        Mock BuildMock(Type typeToMock);
+    }
+}
