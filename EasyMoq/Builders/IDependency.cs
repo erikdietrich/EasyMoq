91cbe576c026bfb2d8fea6a635647d353dd836f6
Erik Dietrich <erik@daedtech.com>
2011-12-23 03:21:02 -0600

    Began reworking the structure of the target builders a bit.

---
+++ EasyMoq/Builders/IDependency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DaedTech.EasyMoq.Builders
+{
+    /// <summary>This defines base behavior for </summary>
+    public interface IDependency
+    {
+        /// <summary>Type of the dependency to be mocked</summary>
+        Type TypeToMock { get; set; }
+        
+        /// <summary>Mock builder to use for constructing a mock</summary>
+        IMockBuilder Builder { get; set; }
+
+        /// <summary>Generates a test double for this dependency</summary>
+        /// <returns>The test double in question</returns>
+        object EmitTestDouble();
+
+    }
+}
