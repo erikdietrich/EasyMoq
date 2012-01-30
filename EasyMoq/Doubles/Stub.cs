ae7943373cdc957752ef0968723ac1870cd1e7be
Erik Dietrich <erik@daedtech.com>
2012-01-30 15:26:05 -0600

    Little more work on the stub class

--- EasyMoq/Doubles/Stub.cs
+++ EasyMoq/Doubles/Stub.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Moq;
 
 namespace DaedTech.EasyMoq.Doubles
 {
@@ -9,10 +10,20 @@ namespace DaedTech.EasyMoq.Doubles
     /// <typeparam name="T"></typeparam>
     public class Stub<T> : Dummy<T>
     {
+        /// <summary>This determines if the object in question can be mocked</summary>
+        private readonly IMoqabilityEvaluator _evaluator = new MoqabilityEvaluator();
+
+
+        /// <summary>This is the Moq-like object property to be used for this stub</summary>
         public override object Object
         {
             get
             {
+                if (_evaluator.IsMoqable<T>())
+                {
+                    var myMock = Activator.CreateInstance(typeof(Mock<>).MakeGenericType(typeof(T))) as Mock;
+                    return myMock.Object;
+                }
                 return base.Object;
             }
         }
--- EasyMoqTest/Doubles/StubTest.cs
+++ EasyMoqTest/Doubles/StubTest.cs
@@ -6,6 +6,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SampleCode;
 using Moq;
 using DaedTech.EasyMoq.Doubles;
+using System.Dynamic;
+using System.Reflection.Emit;
 
 namespace DaedTech.EasyMoqTest.Doubles
 {
@@ -20,6 +22,27 @@ namespace DaedTech.EasyMoqTest.Doubles
             public void Is_DefaultInt_For_Int_Generic()
             {
                 Assert.AreEqual<int>(default(int), (int)new Stub<int>().Object);
+            }
+
+            /// <summary>If given a moq-able interface, we should return an implementation of the interface</summary>
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Is_InstanceOfInterface_For_InterfaceArgument()
+            {
+                Assert.IsInstanceOfType(new Stub<IComparable>().Object, typeof(IComparable));
+            }
+
+            /// <summary>If this can be moq-ed, we want it to be, even for class instead of an interface</summary>
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Is_Instance_Of_Base_Class_For_Moqable_Type()
+            {
+                Assert.IsInstanceOfType(new Stub<StubTest>().Object, typeof(StubTest));
+            }
+
+            /// <summary>For now, we're going to return null in this situation</summary>
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Is_Null_For_Non_Moqable_Reference_Type()
+            {
+                Assert.IsNull(new Stub<string>().Object);
             }
         }
     }
